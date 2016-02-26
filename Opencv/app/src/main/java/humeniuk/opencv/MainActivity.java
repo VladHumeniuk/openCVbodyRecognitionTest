d8428191f9e703726630cf75efada9b209e725c8
Vlad Humeniuk <vlad.groovy@gmail.com>
2016-02-26 18:08:06 +0200

    add circle detection

--- Opencv/app/src/main/java/humeniuk/opencv/MainActivity.java
+++ Opencv/app/src/main/java/humeniuk/opencv/MainActivity.java
@@ -9,11 +9,13 @@ import android.view.WindowManager;
 
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
@@ -99,6 +101,11 @@ public class MainActivity extends Activity {
 
         @Override
         public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+//            return detectFullBody(inputFrame);
+            return detectColorCircles(inputFrame);
+        }
+
+        private Mat detectFullBody(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
             long time = System.currentTimeMillis();
             Log.d("testCVframe","start");
             Mat mat = inputFrame.rgba();
@@ -114,6 +121,52 @@ public class MainActivity extends Activity {
 
             return mat;
         }
+
+        private Mat detectColorCircles(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+            Mat mat = inputFrame.rgba();
+            Mat grayMat = new Mat();
+
+            int colorChannels = (mat.channels() == 3) ? Imgproc.COLOR_BGR2GRAY
+                    : ((mat.channels() == 4) ? Imgproc.COLOR_BGRA2GRAY : 1);
+
+            Imgproc.cvtColor(mat, grayMat, colorChannels);
+
+
+            Imgproc.GaussianBlur(grayMat, grayMat, new Size(9, 9), 2, 2);
+
+            double dp = 1.2d;
+            double minDist = 100;
+
+            int minRadius = 0, maxRadius = 0;
+            double param1 = 70, param2 = 72;
+
+            Mat circles = new Mat(420,
+                    300, CvType.CV_8UC1);
+
+            Imgproc.HoughCircles(grayMat, circles,
+                    Imgproc.CV_HOUGH_GRADIENT, dp, minDist, param1,
+                    param2, minRadius, maxRadius);
+
+            int numberOfCircles = (circles.rows() == 0) ? 0 : circles.cols();
+
+            for (int i=0; i<numberOfCircles; i++) {
+
+                double[] circleCoordinates = circles.get(0, i);
+
+
+                int x = (int) circleCoordinates[0], y = (int) circleCoordinates[1];
+
+                Point center = new Point(x, y);
+
+                int radius = (int) circleCoordinates[2];
+                Imgproc.circle(mat, center, radius, new Scalar(0,
+                        255, 0), 4);
+            }
+
+            Log.d("testCV","circles " + numberOfCircles);
+            return mat;
+        }
     };
+
     //http://coding-robin.de/2013/07/22/train-your-own-opencv-haar-classifier.html - haar classifier creation
 }
